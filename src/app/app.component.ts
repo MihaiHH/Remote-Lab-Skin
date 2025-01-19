1d574eaa4cd7cb2a97c1d0ee746e96fb91e39716
Mihai Csipak <mihaiteruelasir@gmail.com>
2025-01-19 13:48:06 +0100

    Añadido Angular Material y creado esqueleto de la página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
