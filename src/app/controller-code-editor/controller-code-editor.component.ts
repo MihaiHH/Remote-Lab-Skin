cb6647a8a9535c8d887981e89ea98c86b3e8f851
Mihai Csipak <mihaiteruelasir@gmail.com>
2025-01-19 21:04:11 +0100

    Añadidos botones de verificar, cargar y compilar codigo

--- src/app/controller-code-editor/controller-code-editor.component.ts
+++ src/app/controller-code-editor/controller-code-editor.component.ts
@@ -3,10 +3,13 @@ import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AVAILABLE_EDITOR_LANGUAGES } from '../../constants/languages.constants';
 import { CamDialogComponent } from '../cam-dialog/cam-dialog.component';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-controller-code-editor',
-  imports: [CodeEditor, FormsModule, CamDialogComponent],
+  imports: [CodeEditor, FormsModule, CamDialogComponent, MatIconModule, MatButtonModule, MatTooltip],
   templateUrl: './controller-code-editor.component.html',
   styleUrl: './controller-code-editor.component.scss',
 })
